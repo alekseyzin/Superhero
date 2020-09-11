1581f25f7ab4174748940887bb62218aebcb545b
Aleksey <aleksey.zin@gmail.com>
2020-09-11 10:37:08 +0300

    Create Alphabetical select component

--- src/app/heroes-page/_components/alphabet/alphabet.component.ts
+++ src/app/heroes-page/_components/alphabet/alphabet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ViewApiUrl} from '../../../shared/services/api';
+import {HeroesService} from '../../../shared/services/heroes.service';
 
 @Component({
   selector: 'app-alphabet',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlphabetComponent implements OnInit {
 
-  constructor() { }
+  modalOpen = false
+  alphabetUrls: Array<string>
+  currentLetter = 'A'
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+    this.getAlphabet()
   }
 
+  getAlphabet(): void {
+    const alphabetUrls = []
+
+    for(let charCode = 97; charCode <= 122; charCode++) {
+      const letter = String.fromCharCode(charCode)
+
+      alphabetUrls.push(letter)
+    }
+      this.alphabetUrls = alphabetUrls
+  }
+
+  toggleModal(): void {
+    this.modalOpen = !this.modalOpen
+  }
+
+  searchByLetter($event: MouseEvent): void {
+    const searchLetter = (<HTMLElement> $event.target).innerText
+    this.heroesService.search(searchLetter).subscribe()
+    this.currentLetter = searchLetter.toLocaleUpperCase()
+    this.toggleModal()
+  }
 }
